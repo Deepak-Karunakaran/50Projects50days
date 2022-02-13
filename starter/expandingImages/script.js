92121a91c6136b301dc3af408f2fd4511c762372
Deepak Karunakaran <deepu.969500@gmail.com>
2022-02-13 23:44:10 +0530

    123

--- starter/expandingImages/script.js
+++ starter/expandingImages/script.js
@@ -1,1 +1,14 @@
+const panel = document.querySelectorAll(".panel");
 
+panel.forEach((el, i, arr) => {
+  el.addEventListener("click", (e) => {
+    removeActivePanel();
+    el.classList.add("active");
+  });
+});
+
+const removeActivePanel = function () {
+  panel.forEach((el) => {
+    el.classList.remove("active");
+  });
+};
