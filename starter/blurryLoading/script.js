8286dc3945a55c1c7050128cb5f74f0993c5688a
Deepak-Karunakaran <76529443+Deepak-Karunakaran@users.noreply.github.com>
2022-02-25 17:02:43 +0530

    123

--- starter/blurryLoading/script.js
+++ starter/blurryLoading/script.js
@@ -1,5 +1,5 @@
 "use strict";
-const loadingText = document.querySelector(".load-text");
+const loadingText = document.querySelector(".loading-text");
 const bg = document.querySelector(".bg");
 
 let load = 0;
@@ -10,5 +10,8 @@ const loadInterval = function () {
     clearInterval(int);
   }
   console.log(load);
+  loadingText.innerText = `${load}%`;
+  loadingText.style.opacity = 1 - load * 0.01;
+  bg.style.filter = `blur(${100 - load}px)`;
 };
 let int = setInterval(loadInterval, 30);
