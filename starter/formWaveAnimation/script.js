916ab9564fe7646b87cfda61f2e5c154afaf349e
Deepak-Karunakaran <76529443+Deepak-Karunakaran@users.noreply.github.com>
2022-03-16 13:03:03 +0530

    werwer

--- starter/formWaveAnimation/script.js
+++ starter/formWaveAnimation/script.js
@@ -1,1 +1,9 @@
+const labels = document.querySelectorAll(".form-control label");
 
+console.log(labels);
+labels.forEach((el, i, li) => {
+  el.innerHTML = el.innerText
+    .split("")
+    .map((el, i) => `<span style='transition-delay:${i * 50}ms'>${el}</span>`)
+    .join("");
+});
