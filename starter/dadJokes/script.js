e9da00fd90f3712e01252f8561fd0fb0d9169a31
Deepak-Karunakaran <76529443+Deepak-Karunakaran@users.noreply.github.com>
2022-03-24 16:32:18 +0530

    123

--- starter/dadJokes/script.js
+++ starter/dadJokes/script.js
@@ -1,1 +1,21 @@
+"use strict";
 
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+const generateJoke = function () {
+  const config = {
+    headers: {
+      accept: "application/json",
+    },
+  };
+  fetch("https://icanhazdadjoke.com/", config)
+    .then((res) => res.json())
+    .then((data) => {
+      jokeEl.innerHTML = data.joke;
+    });
+};
+
+jokeBtn.addEventListener("click", generateJoke);
+
+// generateJoke();
